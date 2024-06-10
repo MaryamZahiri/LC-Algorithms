4bbc93d84845b2eeb31b58eff27f2f5325c3acb3
MaryamZahiri <maryam.zahiri@alumni.ubc.ca>
2024-06-10 12:01:44 -0700

    stack

---
+++ Data_Structure/stack.py
@@ -0,0 +1,31 @@
+'''
+LIFO: Last In First Out
+Time Complexity O(1): a stack limits the insert (push) and remove (pop) operation to only one end of the data structure, i.e., to the top
+Python lists are implemented as dynamic arrays
+'''
+class stack:
+    def __init__(self, initList=[]):
+        self.stack = initList
+
+    def push(self, element):
+        self.stack.append(element)
+        return self.stack
+    
+    def pop(self):
+        return self.stack.pop()
+    
+    def check_stack(self):
+        print(self.stack)
+
+    def check_top(self):
+        print(self.stack[-1])
+    
+
+stack_ds = stack()
+stack_ds.push(1)
+stack_ds.push(2)
+stack_ds.push(3)
+stack_ds.check_stack()
+stack_removed = stack_ds.pop()
+stack_ds.check_stack()
+stack_ds.check_top()
