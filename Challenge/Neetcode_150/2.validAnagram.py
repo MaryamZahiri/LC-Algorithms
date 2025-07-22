376e88746622e9832af7fa02105bcdbe69e4f6c0
Maryam Zahiri <maryam.zahiri@alumni.ubc.ca>
2025-07-22 11:00:22 -0700

    valid anagram

--- Challenge/Neetcode_150/2.validAnagram.py
+++ Challenge/Neetcode_150/2.validAnagram.py
@@ -1,39 +1,44 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t): return False
-        
         counter = {}
         for i in range(len(s)):
-            if s[i] not in counter: counter[s[i]] = 0
-            if t[i] not in counter: counter[t[i]] = 0
+            if not s[i] in counter: counter[s[i]] = 0
+            if not t[i] in counter: counter[t[i]] = 0
             counter[s[i]] += 1
             counter[t[i]] -= 1
-        
-        # check if it is all match
         for char in counter:
             if counter[char] != 0: return False
-
         return True
 
-    def isAnagramBF(self, s: str, t: str) -> bool:
-        # length check
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t): return False
-
-        # match check
-        matched_list = list(t)
-        for s_char in s:
-            found = False
-            for i in range(len(matched_list)):
-                if s_char == matched_list[i]:
-                    matched_list[i] = None # mark as used
-                    found = True
-                    break
-            if not found: return found
-        
-        return found
+        counter = [0] * 26
+        for i in range(len(s)):
+            counter[ord(s[i]) - ord('a')] += 1
+            counter[ord(t[i]) - ord('a')] -= 1
+        for i in range(len(s)):
+            if counter[i] != 0: return False
+        return True
 
     def isAnagramSort(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
+    def isAnagramBF(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+        list_t = list(t)
+
+        for char in s:
+            flag = False
+            for i in range(len(list_t)):
+                # check if it is matched
+                if char == list_t[i]:
+                    list_t[i] = None # mark as used
+                    flag = True
+                    break
+            # no match
+            if not flag: return False
+
+        return True
         
 '''
 1. BF: T: O(n^2) S: O(n)
