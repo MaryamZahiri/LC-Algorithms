73457ca87c66873d74d20ef74eb43da880a60205
Maryam Zahiri <maryam.zahiri@alumni.ubc.ca>
2025-08-18 21:02:59 -0700

    vaild parantheses

---
+++ Challenge/Neetcode_150/021.valid_parantheses.py
@@ -0,0 +1,43 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 == 1: return False
+
+        pair = {
+            ")": "(",
+            "]": "[",
+            "}": "{"
+        }
+        stack = []
+        for char in s:
+            # close
+            if char in pair:
+                if stack and stack[-1] == pair[char]: stack.pop()
+                else: return False
+            # open
+            else:
+                stack.append(char)
+
+        return True if not stack else False
+        """
+        T: O(N) S: O(N)
+        """
+"""
+( -> False
+({) -> False
+([)] -> F
+() -> True
+([]) -> T
+()[] -> T
+"""
+"""
+() 
+OPEN:
+stack
+[
+    (
+]
+
+CLOSE:
+pop -> (
+):(
+"""
